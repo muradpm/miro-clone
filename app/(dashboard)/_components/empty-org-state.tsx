e74bbd34127aafa5fbf65e7a9f7a2ecc0d361bfe
muradpm <abdulkadyr0v@yandex.ru>
2024-04-09 18:51:57 +0300

    add empty org state

---
+++ app/(dashboard)/_components/empty-org-state.tsx
@@ -0,0 +1,27 @@
+import { Building2 } from "lucide-react";
+import { CreateOrganization } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+
+export const EmptyOrgState = () => {
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <Building2 className="h-10 w-10" />
+      <h2 className="text-2xl font-semibold mt-6">No organizations yet</h2>
+      <p className="text-muted-foreground text-sm mt-2">
+        Create an organization to get started
+      </p>
+      <div className="mt-6">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="lg">Create organization</Button>
+          </DialogTrigger>
+          <DialogContent className="p-0 bg-transparent border-none max-w-[510px]">
+            <CreateOrganization />
+          </DialogContent>
+        </Dialog>
+      </div>
+    </div>
+  );
+};
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,17 @@
+"use client";
+
+import { EmptyOrgState } from "./_components/empty-org-state";
+
+import { useOrganization } from "@clerk/nextjs";
+
 const DashboardPage = () => {
-  return <div>Dashboard Page</div>;
+  const { organization } = useOrganization();
+
+  return (
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? <EmptyOrgState /> : <p>Board List</p>}
+    </div>
+  );
 };
 
 export default DashboardPage;
