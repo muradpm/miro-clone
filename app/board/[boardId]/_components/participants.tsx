2ad78275f11472b413dc6efba0788d4ea2367dbe
muradpm <abdulkadyr0v@yandex.ru>
2024-04-13 17:46:34 +0300

    add loading to board layout

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,5 +1,3 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
 export const Participants = () => {
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md px-3 flex items-center shadow-md">
@@ -10,8 +8,6 @@ export const Participants = () => {
 
 Participants.Skeleton = function ParticipantsSkeleton() {
   return (
-    <div className="absolute h-12 top-2 right-2 bg-white rounded-md px-3 flex items-center shadow-md w-[100px]">
-      <Skeleton className="h-full w-full bg-muted-400" />
-    </div>
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md px-3 flex items-center shadow-md w-[100px]" />
   );
 };
--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -19,8 +19,6 @@ export const Toolbar = () => {
 
 Toolbar.Skeleton = function ToolbarSkeleton() {
   return (
-    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] rounded-md shadow-md">
-      <Skeleton className="h-full w-full bg-muted-400" />
-    </div>
+    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] rounded-md shadow-md" />
   );
 };
