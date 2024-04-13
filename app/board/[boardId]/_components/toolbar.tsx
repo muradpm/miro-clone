8607d4b774bc0896151837cd2aeeced83909a6e8
muradpm <abdulkadyr0v@yandex.ru>
2024-04-13 23:48:12 +0300

    add toolbar buttons

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,15 +1,47 @@
+import { ToolButton } from "./tool-button";
+
+import {
+  MousePointer2,
+  Type,
+  StickyNote,
+  Square,
+  Circle,
+  Pen,
+  Eraser,
+  Undo2,
+  Redo2,
+} from "lucide-react";
+
 export const Toolbar = () => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
       <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-        <div>Pencil</div>
-        <div>Square</div>
-        <div>Circle</div>
-        <div>Triangle</div>
+        <ToolButton
+          label="Select"
+          icon={MousePointer2}
+          onClick={() => {}}
+          isActive={true}
+        />
+        <ToolButton label="Text" icon={Type} onClick={() => {}} />
+        <ToolButton label="Sticky note" icon={StickyNote} onClick={() => {}} />
+        <ToolButton label="Rectangle" icon={Square} onClick={() => {}} />
+        <ToolButton label="Ellipse" icon={Circle} onClick={() => {}} />
+        <ToolButton label="Pen" icon={Pen} onClick={() => {}} />
+        <ToolButton label="Eraser" icon={Eraser} onClick={() => {}} />
       </div>
       <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
-        <div>Undo</div>
-        <div>Redo</div>
+        <ToolButton
+          label="Undo"
+          icon={Undo2}
+          onClick={() => {}}
+          isDisabled={true}
+        />
+        <ToolButton
+          label="Redo"
+          icon={Redo2}
+          onClick={() => {}}
+          isDisabled={true}
+        />
       </div>
     </div>
   );
